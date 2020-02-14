5e98882797d1ba2987b1faa173d0326b8708bef3
Mateus Leandro da Silva Vieira <mateus.lsvieira@dominio.local>
2020-02-14 09:26:11 -0300

    config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'ccd79e5141954f',
+        pass: '70ee81757da1cf',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
