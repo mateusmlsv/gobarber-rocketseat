74af51fd32cd3a05fbe0d797cd03dee971e51964
Mateus Leandro da Silva Vieira <mateus.lsvieira@dominio.local>
2020-02-18 09:51:32 -0300

    .env

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: 'ccd79e5141954f',
-        pass: '70ee81757da1cf',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
